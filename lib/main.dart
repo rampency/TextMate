a12d9c382334a293e16fa30d445d65ed0e690277
Vijay Gopal <vgopal5@ford.com>
2017-10-19 13:48:24 -0400

    TextMate first app using Flutter and Dart inital commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "TextMate",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("TextMate")),
+    );
+  }
+}
